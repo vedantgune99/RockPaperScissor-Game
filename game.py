758a9e6c7f2836d8f4d5e56c64885264ab798f3c
Vedant <vedantgune41@gmail.com>
2021-09-05 21:27:57 +0530

    Buttons Added

--- game.py
+++ game.py
@@ -4,4 +4,17 @@ from tkinter import *
 root = Tk()
 root.geometry("800x600")
 root.title("Rock Paper Scissors Game")
+
+# Buttons to select Rock, Paper, Scissors.
+frame = Frame(root, bg="grey")
+
+Button(frame, text="Rock", bg="light blue",
+       width=20, height=2).pack(pady=8)
+Button(frame, text="Paper", bg="pink", width=20,
+       height=2).pack(pady=8)
+Button(frame, text="Scissor", bg="light green",
+       width=20, height=2).pack(pady=8)
+
+frame.pack(fill=X, pady=20)
+
 root.mainloop()
